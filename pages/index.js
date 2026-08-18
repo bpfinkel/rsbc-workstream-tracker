d78fb0b0bd7889cfc6de313fb438fb4f8df5293e
bpfinkel <bfinkel.rsbc@gmail.com>
2026-08-17 21:48:51 -0400

    Add My Account nav link to Tasks page

--- pages/index.js
+++ pages/index.js
@@ -277,6 +277,7 @@ export default function Home() {
         <nav className="page-nav">
           <Link href="/" className="page-nav-link active">Tasks</Link>
           <Link href="/roster" className="page-nav-link">Roster</Link>
+          <Link href="/change-password" className="page-nav-link">My Account</Link>
           <button type="button" className="page-nav-link" onClick={handleSignOut} style={{ background: 'none', border: 'none', cursor: 'pointer', fontFamily: 'inherit' }}>Sign Out</button>
         </nav>
         <button className="btn-primary" onClick={openAddModal}>+ Add Task</button>
