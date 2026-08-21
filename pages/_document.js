d0503f6bda886244b31e8e0f119711c78f5868e3
bpfinkel <bfinkel.rsbc@gmail.com>
2026-08-21 14:59:03 -0400

    Add browser-tab favicon and iOS apple-touch-icon link tags site-wide

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="icon" type="image/png" href="/api/favicon" />
+        <link rel="apple-touch-icon" href="/api/apple-touch-icon" />
       </Head>
       <body>
         <Main />
